50c99bbb3bb2d16f45941eff4c36d6ad16d5763a
Muhammadsodiq <muhammadsodiqkoziyev1@gmail.com>
2024-07-25 15:10:27 +0500

    New Portfolio desing

---
+++ data/index.ts
@@ -0,0 +1,208 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a JS Animation library",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Mediapark - E-commerce Online Shop",
+    des: "An optimized online shopping experience with Next.js SSR",
+    img: "/mediapark.png",
+    iconLists: ["/next.svg", "/re.svg", "/tail.svg", "/ts.svg", "/fm.svg"],
+    link: "https://mediapark.uz",
+  },
+  {
+    id: 2,
+    title: "Avtoritet Service",
+    des: "Experience premium automotive services powered by Vite and React.js, ensuring a fast, modern, and efficient user experience.",
+    img: "/avtoservice.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "http://avtoritetservice.uz",
+  },
+  {
+    id: 3,
+    title: "Elessi - Fresh Vegetables and Fruits Online Shop",
+    des: "Elessi offers a convenient online shopping experience for fresh vegetables and fruits, ensuring swift delivery and top-notch quality from farm to table.",
+    img: "/elessi.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "https://react-exam-elessi.vercel.app/",
+  },
+  {
+    id: 4,
+    title: "Animated Apple Iphone 3D Website",
+    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    img: "/p4.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "https://apple-iphone-omega.vercel.app",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "I've never met a web developer who truly cares about their clients' success like Muhammadsodiq does.",
+    name: "Islam Tashpulatov",
+    title: "Senior Developer of MediaPark",
+  },
+  {
+    quote:
+      "I thought it was impossible to make a website as beautiful as our product, but Muhammadsodiq proved me wrong.",
+    name: "Samandar Bohodirov",
+    title: "Middle Frontend Developer of Orient Auto",
+  },
+  {
+    quote:
+      "After Muhammadsodiq optimized our website, our traffic increased. We can't thank him enough!",
+    name: "Aziz Rakhim",
+    title: "Middle Frontend Developer of MediaPark",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Engineer",
+    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Animation Website",
+    desc: "An animation website is a digital platform where users can create, share, and explore animated content, utilizing tools and resources to design engaging visuals and interactive experiences.",
+    className: "md:col-span-2",
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "CRM",
+    desc: "Intuitive Navigation, Comprehensive Functionality, Data Visualization, User-Friendly Interface",
+    className: "md:col-span-2",
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Middle Frontend Developer",
+    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/muhammadsodiq07",
+  },
+  {
+    id: 2,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/muhammadsodiq-koziyev-50332b242",
+  },
+  {
+    id: 3,
+    img: "/twit.svg",
+    link: "",
+  },
+];
